67b6f4c8bef49850bb6187890849bf36c24574a5
mukanovaakmaral <126714295+mukanovaakmaral@users.noreply.github.com>
2023-04-07 00:01:31 +0600

    Add files via upload

---
+++ kassa-qr.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-kassa-qr',
+  templateUrl: 'kassa-qr.component.html',
+  styleUrls: ['kassa-qr.component.scss']
+})
+
+
+export class KassaQrComponent{
+
+}
