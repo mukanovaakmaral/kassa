a4651998a7044b0ed0896b04d7ab216772280dc7
mukanovaakmaral <126714295+mukanovaakmaral@users.noreply.github.com>
2023-04-07 18:56:44 +0600

    Update kassa-qr.component.ts

--- kassa-qr.component.ts
+++ kassa-qr.component.ts
@@ -1,12 +1,32 @@
-import { Component } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
+
 
 @Component({
-  selector: 'app-kassa-qr',
-  templateUrl: 'kassa-qr.component.html',
-  styleUrls: ['kassa-qr.component.scss']
+  selector: 'app-kaspi-qr',
+  templateUrl: 'kaspi-qr.component.html',
+  styleUrls: ['kaspi-qr.component.scss']
 })
 
 
-export class KassaQrComponent{
+export class KaspiQrComponent {
+  amount = 50000;
+  token = '887777';
 
+  status = Boolean;
+
+  error = 'Ошибка';
+
+  constructor() {
+
+    const error = 'Отмена покупки';
+
+
+  }
+
+  onInit() {
+    if (status === this.token) {
+
+
+    }
+  }
 }
